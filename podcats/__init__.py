326742bc0fe35fe0c85b057260f75230e2be4452
Paul Wiggett <pwiggett@vmware.com>
2024-11-11 14:20:29 +0000

    external

--- podcats/__init__.py
+++ podcats/__init__.py
@@ -127,11 +127,7 @@ class Episode(object):
     @property
     def title(self):
         """Return episode title"""
-        text = os.path.splitext(os.path.basename(self.filename))[0]
-        if self.id3 is not None:
-            val = self.id3.getall('TIT2')
-            if len(val) > 0:
-                text = str(val[0])
+        text = os.path.splitext(os.path.basename(self.filename))
         return text
 
     @property
@@ -142,7 +138,7 @@ class Episode(object):
     @property
     def date(self):
         """Return episode date as unix timestamp"""
-        dt = self.get_tag('date')
+        dt = os.path.splitext(os.path.basename(self.filename))[0].split(' - ')[0]
         if dt:
             formats = [
                 '%Y-%m-%d:%H:%M:%S',
